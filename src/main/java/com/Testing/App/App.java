9f8ba55a3adac824d073e25f972908522e8bdb0e
Chandra Kiran Saladi <chandra.kiran.saladi@ericsson.com>
2019-07-10 11:02:54 -0500

    Testing Maven with CI/CD Travis and Jenkins

---
+++ src/main/java/com/Testing/App/App.java
@@ -0,0 +1,18 @@
+package com.Testing.App;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(args[0]);
+    }
+}
